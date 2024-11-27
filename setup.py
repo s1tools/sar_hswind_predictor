b413c647d725d47b2ef61d81100cc9a1dd3b5026
abenchaabane <abenchaabane@groupcls.com>
2024-11-27 14:04:29 +0100

    moved

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+
+# Get long description text from README.rst.
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('VERSION.txt', 'r') as f:
+    version = f.readline()[:-1]
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="s1_hswind_predictor",
+    version=version,
+    author="Amine Benchaabane",
+    author_email="abenchaabane@groupcls.com",
+    description=("Estimate Hs wind Sea from official L2 OCN product with deep learning model"),
+    license="",
+    url="https://gitlab.brest.cls.fr/abenchaabane/sar_hswind_predictor",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=requirements,
+    long_description=readme,
+)
