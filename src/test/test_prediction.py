25bcbe38ec9e02f096286431d1206ea8944d35c6
abenchaabane <abenchaabane@groupcls.com>
2024-12-03 22:40:19 +0100

    improve script

--- src/test/test_prediction.py
+++ src/test/test_prediction.py
@@ -2,8 +2,9 @@ import os
 from glob import glob
 import numpy as np
 import json
-from inference.run_inference import get_inference
-from inference import config
+
+from hswind_inference.run_inference import get_inference
+from hswind_inference import config
 
 
 def test_hsWindsea_predictions_from_ocn_file():
@@ -15,17 +16,18 @@ def test_hsWindsea_predictions_from_ocn_file():
     input_test_dataset = glob(os.path.join(
         os.path.dirname(__file__),
         "referencedata",
-        '*', 'measurement',
+        'S1A_WV_OCN__2SSV_20231122T063708_20231122T065756_051326_063161_3DF2.SAFE',
+        '**',
         "*.nc"), recursive=True)
 
-    # TODO to be replaced by the official with a particular version
+    # TODO : to be replaced by the official with a particular version
     input_aux_ml2 = os.path.join(os.path.dirname(__file__),
                                  "referencedata",
                                  "S1A_AUX_ML2_V20140406T133000_G20240429T091835.SAFE")
 
     path_ref_results = os.path.join(os.path.dirname(__file__),
                                     "reference_results",
-                                    "s1_HsWindSea_inference_20241126-223213.json")
+                                    "s1_HsWindSea_inference.json")
     with open(path_ref_results) as ref_json:
         ref_results = json.load(ref_json)
 
@@ -34,7 +36,7 @@ def test_hsWindsea_predictions_from_ocn_file():
                                  outdir=config.filesystem.OUTPUT_FOLDER,
                                  aux_ml2=input_aux_ml2,
                                  verbosity=config.INFERENCE.VERBOSITY,
-                                 no_log=True,
+                                 log=False,
                                  no_json=False,
                                  export_result=False)
 
@@ -49,14 +51,14 @@ def test_hsWindsea_predictions_from_ocn_file():
                 expected_phs0 = ref_results['pred_phs0_from_ww3_partitions'][_kk]
                 break
 
-        # _____ check for hsWindsea from ww3 wave spectum
+        # _____ check for hsWindsea from ww3 wave spectrum
         assert np.allclose(infer_hsWind_spec, expected_hsWind_Spec,
                            atol=1e-02)
 
         # _____ check for hsWindSea from ww3 Hs wind partitions
         assert np.allclose(infer_psh0, expected_phs0,
                            atol=1e-02)
-    print('--' * 12, ' Tests for Hs Wind Sea inference are OK', '--' * 12)
+    print('==' * 12, ' Tests for Hs Wind Sea inference are OK', '==' * 12)
 
 
 if __name__ == "__main__":
